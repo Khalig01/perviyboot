c2bf021ab3f31a2ed3aeaf53c022576831a791f3
Khal01 <aliyev_56@list.ru>
2023-12-25 01:01:09 +0300

    bot finish

--- src/main/java/service/CalculatorService.java
+++ src/main/java/service/CalculatorService.java
@@ -1,5 +1,19 @@
 package service;
 
 public class CalculatorService {
+    public int plus(int num1, int num2) {
+        return num1 + num2;
+    }
 
+    public int minus(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public double divide(int num1, int num2) {
+        return (double) num1 / num2;
+    }
 }
